3b1df1bdcffb2a029e7315d2a5b774f4c5e32cd8
romansparta <chenwill98@gmail.com>
2018-08-17 22:51:32 -0400

    Added tokens

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
+import random
 from flask import Flask, request
+from pymessenger.bot import Bot
 
 app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])
+ACCESS_TOKEN = 'EAAdqRpR1iwoBAGY4q5NVUusZBUu22XcgRuZBvXgz8Bo38s9LZALmXZAuDx0pAzehhWvFuIVNKNJ4BinLSZCrDIkNx3MZAK4v4IHbUpRKCEkBaGLEOHpFKkdI7WuU8F5u3LHs3rz9El4ISUj24bQflloTA26MYZB5aLRNAlwekpJ6gZDZD'
+VERIFY_TOKEN = 'itiswednesday'
+bot = Bot(ACCESS_TOKEN)
+
+#We will receive messages that Facebook sends our bot at this endpoint
+@app.route("/", methods=['GET', 'POST'])
 def receive_message():
-    return "Ligma!"
-
-if __name__ == '__main__':
-    app.run()
-
-if request.method == 'GET':
-    token_sent = request.args.get("hub.verify_token")
-    return verify_fb_token(token_sent)
-else:
+    if request.method == 'GET':
+        """Before allowing people to message your bot, Facebook has implemented a verify token
+        that confirms all requests that your bot receives came from Facebook."""
+        token_sent = request.args.get("hub.verify_token")
+        return verify_fb_token(token_sent)
+    #if the request was not get, it must be POST and we can just proceed with sending a message back to user
+    else:
         # get whatever message a user sent the bot
        output = request.get_json()
        for event in output['entry']:
@@ -28,3 +33,27 @@ else:
                     response_sent_nontext = get_message()
                     send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
+
+
+def verify_fb_token(token_sent):
+    #take token sent by facebook and verify it matches the verify token you sent
+    #if they match, allow the request, else return an error
+    if token_sent == VERIFY_TOKEN:
+        return request.args.get("hub.challenge")
+    return 'Invalid verification token'
+
+
+#chooses a random message to send to the user
+def get_message():
+    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
+    # return selected item to the user
+    return random.choice(sample_responses)
+
+#uses PyMessenger to send response to user
+def send_message(recipient_id, response):
+    #sends user the text message provided via input response parameter
+    bot.send_text_message(recipient_id, response)
+    return "success"
+
+if __name__ == "__main__":
+    app.run()
